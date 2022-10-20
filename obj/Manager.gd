77901a04e8a58ed71d5def2ff2a50aa6af654c42
TornoM24 <54222500+TornoM24@users.noreply.github.com>
2022-10-20 13:37:05 -0700

    z

--- obj/Manager.gd
+++ obj/Manager.gd
@@ -1,16 +1,14 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var movementLocked = false
 
-# Called when the node enters the scene tree for the first time.
+####################
+#Party Management  #
+####################
+var party = [[],[],[]]
+var inventory = []
+
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
